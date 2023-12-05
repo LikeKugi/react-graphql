14137cd5a667afbbd1293a99cae7220229cbe476
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-12-05 04:28:30 +0300

    feat: add playground for code

--- webdev-graphql/src/App.tsx
+++ webdev-graphql/src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { graphql } from 'cm6-graphql';
+import CodeMirror from '@uiw/react-codemirror';
+import './App.css';
+import styles from './styles/Code.module.scss';
+import React, { useCallback, useState } from 'react';
+import { vscodeDark } from '@uiw/codemirror-theme-vscode';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState('console.log(\'hello world!\');');
+  const onChange = useCallback((val, viewUpdate) => {
+    console.log(viewUpdate);
+    setValue(val);
+  }, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [codeWidth, setCodeWidth] = useState(40)
+  const [codeHeight, setCodeHeight] = useState(60);
+
+  const handleMouseDown = (e:  React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!(e.nativeEvent.target instanceof HTMLElement)) return
+    if (
+      e.nativeEvent.offsetX < 5 &&
+      e.nativeEvent.target.className === styles.code__border
+    ) {
+      const m_pos = e.nativeEvent.x;
+      document.addEventListener("mousemove", resize, false);
+    }
+  }
+
+
+  return (<div className={styles.code}>
+    <CodeMirror value={value}
+                height={`${codeHeight}vh`}
+                width={`${codeWidth}vw`}
+                theme={vscodeDark}
+                extensions={[graphql()]}
+                onChange={onChange}/>
+    <div className={styles.code__border} onMouseDown={handleMouseDown} />
+  </div>);
 }
 
-export default App
+export default App;
